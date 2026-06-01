5fbc9729f01a2b8ac968286b4dc45efa09f33340
Adam Alexander <46898882+aja26@users.noreply.github.com>
2026-06-02 00:50:18 +0100

    update

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const locomotiveScroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
 });
 
-console.log(LocomotiveScroll.version);
+const rail = document.querySelector('.rail__inner');
+
+locomotiveScroll.on('scroll', ({ scroll }) => {
+  rail.style.transform = `translate3d(${-scroll.y * 0.4}px, 0, 0)`;
+  console.log(scroll.y);
+});
