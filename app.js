a572115555e5b6efc7cd35b717ace4669c19545e
Adam Alexander <46898882+aja26@users.noreply.github.com>
2026-07-31 23:17:11 +0100

    UPDATE

--- app.js
+++ app.js
@@ -1,74 +1,85 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-let direction = 1; // 1 = forward, -1 = backward scroll
+// let direction = 1; // 1 = forward, -1 = backward scroll
 
-const roll1 = roll(".big-name .name-wrap", {duration: 18}),
-      scroll = ScrollTrigger.create({
-        onUpdate(self) {
-          if (self.direction !== direction) {
-            direction *= -1;
-            gsap.to([roll1], {timeScale: direction, overwrite: true});
-          }
-        }
-      });
+// const roll1 = roll(".big-name .name-wrap", {duration: 18}),
+//       scroll = ScrollTrigger.create({
+//         onUpdate(self) {
+//           if (self.direction !== direction) {
+//             direction *= -1;
+//             gsap.to([roll1], {timeScale: direction, overwrite: true});
+//           }
+//         }
+//       });
 
-// helper function that clones the targets, places them next to the original, then animates the xPercent in a loop to make it appear to roll across the screen in a seamless loop.
-function roll(targets, vars, reverse) {
-  vars = vars || {};
-  vars.ease || (vars.ease = "none");
-  const tl = gsap.timeline({
-          repeat: -1,
-          onReverseComplete() { 
-            this.totalTime(this.rawTime() + this.duration() * 10); // otherwise when the playhead gets back to the beginning, it'd stop. So push the playhead forward 10 iterations (it could be any number)
-          }
-        }), 
-        elements = gsap.utils.toArray(targets),
-        clones = elements.map(el => {
-          let clone = el.cloneNode(true);
-          el.parentNode.appendChild(clone);
-          return clone;
-        }),
-        positionClones = () => elements.forEach((el, i) => gsap.set(clones[i], {position: "absolute", overwrite: false, top: el.offsetTop, left: el.offsetLeft + (reverse ? -el.offsetWidth : el.offsetWidth)}));
-  positionClones();
-  elements.forEach((el, i) => tl.to([el, clones[i]], {xPercent: reverse ? 100 : -100, ...vars}, 0));
-  window.addEventListener("resize", () => {
-    let time = tl.totalTime(); // record the current time
-    tl.totalTime(0); // rewind and clear out the timeline
-    positionClones(); // reposition
-    tl.totalTime(time); // jump back to the proper time
-  });
-  return tl;
+// // helper function that clones the targets, places them next to the original, then animates the xPercent in a loop to make it appear to roll across the screen in a seamless loop.
+// function roll(targets, vars, reverse) {
+//   vars = vars || {};
+//   vars.ease || (vars.ease = "none");
+//   const tl = gsap.timeline({
+//           repeat: -1,
+//           onReverseComplete() { 
+//             this.totalTime(this.rawTime() + this.duration() * 10); // otherwise when the playhead gets back to the beginning, it'd stop. So push the playhead forward 10 iterations (it could be any number)
+//           }
+//         }), 
+//         elements = gsap.utils.toArray(targets),
+//         clones = elements.map(el => {
+//           let clone = el.cloneNode(true);
+//           el.parentNode.appendChild(clone);
+//           return clone;
+//         }),
+//         positionClones = () => elements.forEach((el, i) => gsap.set(clones[i], {position: "absolute", overwrite: false, top: el.offsetTop, left: el.offsetLeft + (reverse ? -el.offsetWidth : el.offsetWidth)}));
+//   positionClones();
+//   elements.forEach((el, i) => tl.to([el, clones[i]], {xPercent: reverse ? 100 : -100, ...vars}, 0));
+//   window.addEventListener("resize", () => {
+//     let time = tl.totalTime(); // record the current time
+//     tl.totalTime(0); // rewind and clear out the timeline
+//     positionClones(); // reposition
+//     tl.totalTime(time); // jump back to the proper time
+//   });
+//   return tl;
+// }
+
+// /**
+//  *  Magnetic buttons
+//  */
+
+
+// /**
+// * Magnetic Buttons
+// */
+// var magnets = document.querySelectorAll('.magnetic')
+// var strength = 50
+
+// magnets.forEach( (magnet) => {
+//   magnet.addEventListener('mousemove', moveMagnet );
+//   magnet.addEventListener('mouseout', function(event) {
+//     TweenMax.to( event.currentTarget, 1, {x: 0, y: 0, ease: Power4.easeOut})
+//   } );
+// });
+
+// function moveMagnet(event) {
+//   var magnetButton = event.currentTarget
+//   var bounding = magnetButton.getBoundingClientRect()
+
+//   //console.log(magnetButton, bounding)
+
+//   TweenMax.to( magnetButton, 1, {
+//     x: ((( event.clientX - bounding.left)/magnetButton.offsetWidth) - 0.5) * strength,
+//     y: ((( event.clientY - bounding.top)/magnetButton.offsetHeight) - 0.5) * strength,
+//     ease: Power4.easeOut
+//   })
+
+//   //magnetButton.style.transform = 'translate(' + (((( event.clientX - bounding.left)/(magnetButton.offsetWidth))) - 0.5) * strength + 'px,'+ (((( event.clientY - bounding.top)/(magnetButton.offsetHeight))) - 0.5) * strength + 'px)';
+// }
+
+function App() {
+  return (
+    <div>
+      <h1>React is working! 🎉</h1>
+      <p>{2 + 2}</p>
+    </div>
+  );
 }
 
-/**
- *  Magnetic buttons
- */
-
-
-/**
-* Magnetic Buttons
-*/
-var magnets = document.querySelectorAll('.magnetic')
-var strength = 50
-
-magnets.forEach( (magnet) => {
-  magnet.addEventListener('mousemove', moveMagnet );
-  magnet.addEventListener('mouseout', function(event) {
-    TweenMax.to( event.currentTarget, 1, {x: 0, y: 0, ease: Power4.easeOut})
-  } );
-});
-
-function moveMagnet(event) {
-  var magnetButton = event.currentTarget
-  var bounding = magnetButton.getBoundingClientRect()
-
-  //console.log(magnetButton, bounding)
-
-  TweenMax.to( magnetButton, 1, {
-    x: ((( event.clientX - bounding.left)/magnetButton.offsetWidth) - 0.5) * strength,
-    y: ((( event.clientY - bounding.top)/magnetButton.offsetHeight) - 0.5) * strength,
-    ease: Power4.easeOut
-  })
-
-  //magnetButton.style.transform = 'translate(' + (((( event.clientX - bounding.left)/(magnetButton.offsetWidth))) - 0.5) * strength + 'px,'+ (((( event.clientY - bounding.top)/(magnetButton.offsetHeight))) - 0.5) * strength + 'px)';
-}
+export default App;
