f72bed6fdfe10d00954bafe2187fd17de38ed4d5
Adam Alexander <46898882+aja26@users.noreply.github.com>
2026-08-03 16:22:26 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </BrowserRouter>
 )
