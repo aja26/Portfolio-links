bafa2a0a5cb938a439814858bc490692706fa76f
Adam Alexander <46898882+aja26@users.noreply.github.com>
2026-08-03 17:06:19 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,74 @@ import { Link } from "react-router-dom";
 export default function Navbar(){
 
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">about</Link>
-            <Link to="/projects">projects</Link>
-            <Link to="/contact">contact</Link>
-        </nav>
+        <header>
+            <nav className="container">
+                 <div className="main-menu">
+                    <ul>
+                        <div className="magnetic">
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                        </div>
+
+                        <div>
+                            <li className="magnetic">
+                                 <Link to="/about">about</Link>
+                            </li>
+                        </div>
+                        <div>
+                            <li className="magnetic">
+                                 <Link to="/projects">projects</Link>
+                            </li>
+                        </div>
+
+                        <div className="magnetic">
+                            <Link to="/contact">contact</Link>
+                        </div>
+                    
+                    </ul>
+                    
+                 </div>
+            </nav>
+        </header>
+
+        
+        
         
     )
     
 }
+
+//  <nav class="container">
+//                 <div class="main-menu">
+//                     <ul>
+//                         <div class="magnetic">
+//                                 <li><a href="index.html">Links</a></li>
+//                         </div>
+                        
+//                         <div class="magnetic">
+//                                 <li><a href="testing.html">Tests</a></li>
+//                         </div>
+
+//                         <div class="magnetic">
+//                             <li><a href="about.html">About</a></li>
+//                         </div>
+
+//                             <div class="magnetic">
+//                                 <li><a href="contact.html">Contact</a></li>
+//                         </div>
+//                     </ul>
+//                 </div>
+               
+//                 <div class="social-menu">
+//                     <ul>
+//                         <div class="magnetic">
+//                             <li><a target="_blank" href="https://www.linkedin.com/in/adam-alexander-91528337/"><i class="ri-linkedin-box-fill"></i></a></li>
+//                         </div>
+//                         <div class="magnetic">
+//                             <li><a target="_blank" href="https://www.behance.net/AdamJA"><i class="ri-behance-line"></i></a></li>
+//                         </div>
+//                     </ul>
+//                 </div>
+        
+//             </nav>
