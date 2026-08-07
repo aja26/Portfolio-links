ce430049f9520d8127a5d1e88f46b3f0351c420b
Adam Alexander <46898882+aja26@users.noreply.github.com>
2026-08-07 23:43:50 +0100

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,16 @@
 export default function About(){
-    return <><h1>About</h1></>
+    return <>
+        <h1>About</h1>
+        <section>
+
+            <p>I am currently a freelance digital designer with over ten years of experience, I specialise in delivering creative and technical solutions for small to medium-sized businesses. My focus lies in crafting impactful digital and print designs, developing custom HTML/CSS websites, and implementing targeted UX/UI improvements to enhance user engagement and optimise search rankings.</p>
+
+            <p>With a proven ability to tackle diverse design challenges, I aim to empower businesses to achieve their unique goals through tailored solutions. My expertise in experience design, landing pages, and multitasking allows me to create meaningful, user-centered designs that align with client objectives and drive results.</p>
+
+            {/* <!-- turn on when going live use  --> */}
+            <a target="_blank" href="#">Download CV</a>
+            {/* <!-- <a target="_blank" href="/media/docs/Adam_Alexander_CV-UXUI-Designer.pdf">Download CV</a> --> */}
+        </section>
+        
+        </>
 }
