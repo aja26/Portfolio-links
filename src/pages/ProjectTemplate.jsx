232b5bccbdb0c303a6b04d3e46356a512c23a299
Adam Alexander <46898882+aja26@users.noreply.github.com>
2026-08-25 23:59:43 +0100

    update

--- src/pages/ProjectTemplate.jsx
+++ src/pages/ProjectTemplate.jsx
@@ -3,6 +3,8 @@
 export default function ProjectTemplate(){
     return <>
     <h1>Project Template</h1>
+
+    {/* Use Behance as template structure */}
     
     <p className="project-desc">Here is the project desc</p>
     </>
