2c25a1051ea2d28c57e793463bfdce783092d789
Adam Alexander <46898882+aja26@users.noreply.github.com>
2026-08-25 23:59:04 +0100

    update

--- src/pages/ProjectTemplate.jsx
+++ src/pages/ProjectTemplate.jsx
@@ -1,5 +1,9 @@
 // The template for each project will run this
 
 export default function ProjectTemplate(){
-    return <><h1>Project Template</h1></>
+    return <>
+    <h1>Project Template</h1>
+    
+    <p className="project-desc">Here is the project desc</p>
+    </>
 }
