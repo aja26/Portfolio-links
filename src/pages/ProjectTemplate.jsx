5ab14628bef46351bc3e79c08c1127e2e2330cf8
Adam Alexander <46898882+aja26@users.noreply.github.com>
2026-08-31 23:14:04 +0100

    update

--- src/pages/ProjectTemplate.jsx
+++ src/pages/ProjectTemplate.jsx
@@ -12,6 +12,9 @@ export default function ProjectTemplate(){
      <section>
         <img src="#" alt="#" />
     </section>
+     <section>
+        <img src="#" alt="#" />
+    </section>
     
 
 
